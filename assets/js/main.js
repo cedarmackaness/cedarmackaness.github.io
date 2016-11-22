270aa1e71de6c8ee5a2d237637d7f8ea054d4400
Maker Doo <matsumaker@gmail.com>
2016-11-22 15:59:26 -0800

    checkin

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,3 +247,7 @@
 	});
 
 })(jQuery);
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
